0f90cd4a2f53187ccefc8f3e32c505b9d669de43
Martijn Boland <martijn@boland.org>
2015-12-10 11:48:43 +0100

    Tweaked webpack configs and added prod config.

--- VSReact.Web/webpack.prod.config.js
+++ VSReact.Web/webpack.prod.config.js
@@ -7,42 +7,36 @@ var pkg = require('./package.json');
 var vendorPackages = Object.keys(pkg.dependencies).filter(function (el) {
   return el.indexOf('font') === -1; // exclude font packages from vendor bundle
 });
+var outputFileTemplateSuffix = '-' + pkg.version;
 
 module.exports = {
   devtool: 'eval',
   entry: {
-    main: [
-      'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server',
-      './app/index'
-    ],
+    main: './app/index',
     vendor: vendorPackages
   },
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]' + outputFileTemplateSuffix + '.js',
+    chunkFilename: '[id]' + outputFileTemplateSuffix + '.js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      __API_URL__: JSON.stringify(process.env.API_URL || '//localhost:51407')
+      __API_URL__: JSON.stringify(process.env.API_URL || 'http://localhost:51407')
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      filename: 'vendor.js',
+      filename: 'vendor' + outputFileTemplateSuffix + '.js',
       minChunks: Infinity
     }),
     new HtmlWebpackPlugin({
       template: 'index.html'
     })
   ],
-  resolveLoader: {
-    'fallback': path.join(__dirname, 'node_modules')
-  },
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['babel'],
       exclude: /node_modules/,
       include: __dirname
     }, {
