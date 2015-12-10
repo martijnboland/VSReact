ea7e055256a75dc453bbb8addd708657414ba09e
Martijn Boland <martijn@boland.org>
2015-12-10 12:38:31 +0100

    Fixed issue with encoding special chars in todomvc css by explicitly setting encoding in index.html to utf-8.

--- VSReact.Web/webpack.config.js
+++ VSReact.Web/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
