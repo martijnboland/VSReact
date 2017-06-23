683756111416ca33ecbaa62fcbeb0b899b65dde9
Martijn Boland <martijn@boland.org>
2017-06-23 16:57:59 +0200

    Re-added production build

--- VSReact.Web/webpack.config.js
+++ VSReact.Web/webpack.config.js
@@ -8,49 +8,79 @@ var pkg = require('./package.json');
 var vendorPackages = Object.keys(pkg.dependencies).filter(function (el) {
   return el.indexOf('font') === -1; // exclude font packages from vendor bundle
 });
+var outputFileTemplateSuffix = '-' + pkg.version;
 
-module.exports = {
-  devServer: {
-    contentBase: './build',
-    host: 'localhost',
-    port: 3000
-  },
-  entry: {
-    main: [
-      'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server',
-      'react-hot-loader/patch',
-      './app/index'
+
+module.exports = function(env) {
+  
+  env = env || {}; 
+  var isProd = env.production === true;
+
+  // Setup base config for all environments
+  var config = {
+    plugins: [
+      new webpack.DefinePlugin({
+        __API_URL__: JSON.stringify(process.env.API_URL || '//localhost:51407')
+      }),
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
     ],
-    vendor: vendorPackages
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].js'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __API_URL__: JSON.stringify(process.env.API_URL || '//localhost:51407')
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: __dirname
+      }, {
+        test: /\.css?$/,
+        use: ['style-loader', 'raw-loader'],
+        include: __dirname
+      }]
+    }
+  }
+
+  // Set environment-specific properties to the config
+  if (isProd) {
+    config.entry = {
+      main: './app/index',
+      vendor: vendorPackages
+    };
+    config.output = {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name]' + outputFileTemplateSuffix + '.js',
+      chunkFilename: '[id]' + outputFileTemplateSuffix + '.js'
+    };
+    config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor' + outputFileTemplateSuffix + '.js',
+      minChunks: Infinity
+    }))
+  } else { // dev 
+    config.devServer = {
+      contentBase: './build',
+      host: 'localhost',
+      port: 3000
+    };
+    config.entry = {
+      main: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        './app/index'
+      ],
+      vendor: vendorPackages
+    };
+    config.output = {
+      path: path.join(__dirname, 'build'),
+      filename: '[name].js'
+    };
+    config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       filename: 'vendor.js',
       minChunks: Infinity
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ],
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      include: __dirname
-    }, {
-      test: /\.css?$/,
-      use: ['style-loader', 'raw-loader'],
-      include: __dirname
-    }]
+    }))
   }
+
+  return config;
 };
