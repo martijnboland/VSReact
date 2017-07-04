498156e215298d8d9ccf295a34971874f99b032b
Martijn Boland <martijn@boland.org>
2017-07-04 12:31:41 +0200

    Added mobx devtools

--- VSReact.Web/app/containers/App.tsx
+++ VSReact.Web/app/containers/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Provider } from 'mobx-react';
 import TodoApp from './TodoApp';
+import DevTools from 'mobx-react-devtools'
 import { AppState } from '../store/AppState';
 import { TodoStore } from '../store/TodoStore';
 
@@ -16,6 +17,7 @@ export default class Root extends React.Component<IRootProps, void> {
       <Provider appState={appState} todoStore={todoStore}>
         <div>
           <TodoApp appState={appState} todoStore={todoStore} />
+          <DevTools />
         </div>
       </Provider>
     );
